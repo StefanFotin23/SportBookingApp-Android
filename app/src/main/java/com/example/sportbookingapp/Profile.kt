9980b77027a4d9642e13aaa162c38b93e8368e69
Stefan <andreifotin@gmail.com>
2023-05-25 19:28:50 +0300

    Finished Profile Fragment

--- app/src/main/java/com/example/sportbookingapp/Profile.kt
+++ app/src/main/java/com/example/sportbookingapp/Profile.kt
@@ -1,5 +1,6 @@
 package com.example.sportbookingapp
 
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
@@ -15,6 +16,7 @@ import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
@@ -33,6 +35,7 @@ private const val ARG_PARAM2 = "param2"
 class Profile : Fragment() {
     private var db = Firebase.firestore
 
+    @SuppressLint("CutPasteId", "SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -47,6 +50,7 @@ class Profile : Fragment() {
             // finish() // close the current activity
         }
 
+        val fullNameTextView = view.findViewById<TextView>(R.id.fullNameTextView)
         db.collection("users")
             .get()
             .addOnSuccessListener { result ->
@@ -58,10 +62,11 @@ class Profile : Fragment() {
                     val phoneNumber = document.getString("phoneNumber")
 
                     // Set the retrieved profile data to the corresponding EditText fields
-                    view.findViewById<EditText>(R.id.emailEditText).setText(email)
+                    view.findViewById<TextView>(R.id.emailTextView).text = email
                     view.findViewById<EditText>(R.id.firstNameEditText).setText(firstName)
                     view.findViewById<EditText>(R.id.lastNameEditText).setText(lastName)
                     view.findViewById<EditText>(R.id.phoneNumberEditText).setText(phoneNumber)
+                    fullNameTextView.text = "Welcome, $firstName $lastName!"
                 }
             }
             .addOnFailureListener { exception ->
@@ -75,24 +80,31 @@ class Profile : Fragment() {
         // Set a click listener on the Save button
         saveButton.setOnClickListener {
             // Get the updated values from the TextView fields
-            val email = view.findViewById<TextView>(R.id.emailEditText).text.toString()
+            val email = view.findViewById<TextView>(R.id.emailTextView).text.toString()
             val firstName = view.findViewById<TextView>(R.id.firstNameEditText).text.toString()
             val lastName = view.findViewById<TextView>(R.id.lastNameEditText).text.toString()
             val phoneNumber = view.findViewById<TextView>(R.id.phoneNumberEditText).text.toString()
 
             // Find the user document with the matching email
             val userQuery = db.collection("users").whereEqualTo("email", email)
-            userQuery.get().addOnSuccessListener { documents ->
-                // There should only be one document matching the query
-                if (documents.size() == 1) {
-                    val userDoc = documents.documents[0]
-                    // Update the existing user document with the new field values
-                    userDoc.reference.update("firstName", firstName)
-                    userDoc.reference.update("lastName", lastName)
-                    userDoc.reference.update("phoneNumber", phoneNumber)
-                } else {
-                    // Handle the case where no user document was found or multiple documents were found
+            if (phoneNumber.length == 10) {
+                userQuery.get().addOnSuccessListener { documents ->
+                    // There should only be one document matching the query
+                    if (documents.size() == 1) {
+                        val userDoc = documents.documents[0]
+                        // Update the existing user document with the new field values
+                        userDoc.reference.update("firstName", firstName)
+                        userDoc.reference.update("lastName", lastName)
+                        userDoc.reference.update("phoneNumber", phoneNumber)
+                    } else {
+                        // Handle the case where no user document was found or multiple documents were found
+                    }
+                    fullNameTextView.text = "Welcome, $firstName $lastName!"
+                    Toast.makeText(context, "Data successfully updated!", Toast.LENGTH_SHORT).show()
                 }
+            } else {
+                Toast.makeText(context, "Phone number should have 10 digits!", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
         return view
